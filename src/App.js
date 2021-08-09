8afb52dd52f8fe3e0eacd8ed7d0b017b0b2ddbbc
Lyderies <lyderies@hotmail.com>
2021-08-09 14:18:09 +1200

    basic mobile styling added

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import Form from "./components/Form";
 
 function App() {
   return (
-    <div className="App">
-      <section>
-        <article>
+    <div className="App container mx-auto w-screen h-screen flex flex-wrap place-items-center">
+      <section className="m-2 p-2 text-center w-full font-BebasNeue">
+        <article className=" text-3xl">
           <h1>Rent Calculator</h1>
         </article>
-        <article>
+        <article className="text-xl">
           <p>Welcome to the rent calculator</p>
         </article>
       </section>
-      <section>
+      <section className="container mx-auto m-2 p-2">
         <Form />
       </section>
     </div>
