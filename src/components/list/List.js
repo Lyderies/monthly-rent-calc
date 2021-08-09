ce0d200ed4d34badde2a28fb8c7c83f48eb9d79f
Lyderies <lyderies@hotmail.com>
2021-08-09 15:44:05 +1200

    list layout and function comments

---
+++ src/components/list/List.js
@@ -0,0 +1,15 @@
+// list to be created from how many times a user chooses for the function to run
+// List will add in dates 1, 3, 6, 9 and 12 times
+// list will be printed out in an easy to read format
+// list will have dates and rent amount
+// list of dates will be an array and rent/amount will also be an array together in the list
+
+// first build 3 months, can only be chosen with fortnight/month?
+
+import React from "react";
+
+const List = () => {
+  return <div></div>;
+};
+
+export default List;
